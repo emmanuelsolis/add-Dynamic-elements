44ebf04b77086145c17b98431b8b3855d1ea7d74
Emmanuel Solis <emma.ruso@outlook.com>
2023-09-14 17:54:32 -0600

    partimos del fomulario con validaciones

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
-/* Verificamos si es un numero   */
-const numRegx = /^[0-9]+$/;
 
-/*
-   console.log("Aqui ando")
-   function validar() {
-   let nombre = document.querySelector('input[name="nombre"]').value;
-   let precioInput = document.getElementById("precio");
-   let precioValor = precioInput.value.trim() /* Eliminamos los espacios */
-/*
-   let precio = document.querySelector('input[name="precio"]').value;
-   if(!numRegx.test(precioValor)){
-     alert("El precio debe ser un numero")
-   }
-   let cantidad = document.querySelector('input[name="cantidad"]').value;
-   let descripcion = document.querySelector( 'textarea[name="descripcion"]' ).value;
-   let categoria = document.querySelector( 'select[name="categoria"]' ).value;
-   let estado = document.querySelector('input[name="estado"]').value;
-
-   if ( nombre == "" || precio == "" || cantidad == "" || descripcion == "" || categoria == "" || estado == "" ) { 
-       alert("Todos los campos son obligatorios");
-   } else {
-       alert("Datos enviados correctamente");
-   }
- } 
- */
 const d = document;
 const validarCampos = (idFormulario) => {
   let listaCampos = d.querySelectorAll(`#${idFormulario} [data-validate]`);
@@ -37,6 +12,11 @@ const validarCampos = (idFormulario) => {
         setTimeout(() => {
           elemento.style.setProperty("border", "");
         }, 2000);
+      }else {
+         elemento.style.setProperty("border", "1px solid green");
+         setTimeout(() => {
+            elemento.style.setProperty("border", "");
+         }, 2000);
       }
 
       if (tipoElemento === "checkbox" && !elemento.checked) {
@@ -45,6 +25,11 @@ const validarCampos = (idFormulario) => {
         setTimeout(() => {
           elemento.style.setProperty("outline", "");
         }, 2000);
+      }else {
+         elemento.style.setProperty("outline", "1px solid green");
+         setTimeout(() => {
+            elemento.style.setProperty("outline", "");
+         }, 2000);
       }
     });
   }
@@ -54,13 +39,9 @@ const validarCampos = (idFormulario) => {
 const enviarFormulario = () => {
   event.preventDefault();
   if (!validarCampos('formValidacion')) {
-    Swal.fire({
-      icon: "error",
-      title: "Oops...",
-      text: "Algo salió mal !",
-      footer: "<h3>Verifique que no falte algún campo</h3>",
-    });
-    return;
+     setTimeout (() => {
+         alert("Todos los campos son obligatorios");
+       }, 1000);
   } else {
     alert("Datos enviados correctamente");
   }
